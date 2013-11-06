6a1e8138daa2f158e8244968624f22b7518b85b2
Song <ysong.sc@gmail.com>
2013-11-06 01:49:45 -0500

    question 12

---
+++ ints_less_than_n_digits.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void print(vector<char>& number){
+    bool is_beginning_zero = true;
+    
+    for(int i=0; i<number.size(); i++){
+	if(number[i] != '0')
+	    if(is_beginning_zero)
+		is_beginning_zero = false;
+
+	if(is_beginning_zero == false)
+	    cout<<number[i];
+    }
+    cout<<" ";
+}
+
+void recursive_print(vector<char> &number, int index){
+    if(index == number.size()-1){
+	print(number);
+	return;
+    }
+    for(int i=0; i<10; i++){
+	number[index+1] = i+'0';
+	recursive_print(number, index+1);
+    }
+}
+
+
+void ints_less_than_n_digits(int n){
+    //totally n bits, if n=2, the number sequence is 0, 1, ... 99
+    if (n <=0 )
+	return;
+    // initialize vector of char 
+    vector<char> number;
+    for(int i=0; i<n; i++){
+	number.push_back('0');
+    }
+    for(int i=0; i<10; i++){
+	number[0] = i+'0';
+	recursive_print(number, 0);
+    }
+    
+}
+
+int main(){
+    ints_less_than_n_digits(2);
+    cout<<endl;
+    ints_less_than_n_digits(3);
+    return 0;
+}
