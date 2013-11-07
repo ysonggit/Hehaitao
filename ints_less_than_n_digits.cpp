62721979f62cbe68abd2cd3ed9f967a462787160
Song <ysong.sc@gmail.com>
2013-11-07 18:13:23 -0500

    add comments on question 11

--- ints_less_than_n_digits.cpp
+++ ints_less_than_n_digits.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 using namespace std;
 
-
+// print the complete number of n bits 
 void print(vector<char>& number){
     bool is_beginning_zero = true;
     
     for(int i=0; i<number.size(); i++){
+	// filter the '0' before non-zero int, like 010
 	if(number[i] != '0')
 	    if(is_beginning_zero)
 		is_beginning_zero = false;
@@ -17,6 +18,13 @@ void print(vector<char>& number){
     cout<<" ";
 }
 
+// recursivel print number from low bit to high bit
+// for example: a decimal number of 3 bits
+// first fix the hundred bit to 0, then fix the tenth bit to 0, reaches the integer bit
+// then iterates 10 times on the integer bit to print 000, 001, 002,...,009
+// and fix the hundred bit to 0, update the tenth bit to 1, reaches the integer bit
+// iterates 10 times to print 010, 011, ..., 019
+// etc.
 void recursive_print(vector<char> &number, int index){
     if(index == number.size()-1){
 	print(number);
@@ -30,7 +38,6 @@ void recursive_print(vector<char> &number, int index){
 
 
 void ints_less_than_n_digits(int n){
-    //totally n bits, if n=2, the number sequence is 0, 1, ... 99
     if (n <=0 )
 	return;
     // initialize vector of char 
@@ -39,15 +46,16 @@ void ints_less_than_n_digits(int n){
 	number.push_back('0');
     }
     for(int i=0; i<10; i++){
-	number[0] = i+'0';
+	number[0] = i+'0'; // ASCII representation
 	recursive_print(number, 0);
     }
     
 }
 
 int main(){
+    int count = 0;
     ints_less_than_n_digits(2);
     cout<<endl;
-    ints_less_than_n_digits(3);
+    //ints_less_than_n_digits(3);
     return 0;
 }
